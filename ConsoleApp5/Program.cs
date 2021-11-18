aac20103c14310327c39d94af040e1d9e0f0595b
Katerina <matomatodance@gmail.com>
2021-11-18 22:31:39 +0300

    First HW in GitHub

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static string DecimalToBinary(uint decimalNumber)
+    {
+        var binaryNumber = string.Empty;
+        while (decimalNumber > 0)
+        {
+            binaryNumber = (decimalNumber % 2) + binaryNumber;
+            decimalNumber /= 2;
+        }
+
+        return binaryNumber;
+    }
+
+    private static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.Write("Введите число в десятичной системе: ");
+        var decNum = uint.Parse(Console.ReadLine());
+        var binNum = DecimalToBinary(decNum);
+        Console.WriteLine("{0} => {1}", decNum, binNum);
+        Console.ReadLine();
+    }
+}
